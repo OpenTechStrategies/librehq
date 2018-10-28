1ad9846c069bfd263c9b796eae59000986c3de53
Frank Duncan <frank@kank.net>
2018-10-28 05:51:40 -0500

    Add librehq-wikis as a submodule.
    Also add some stubs and the various plumbing to import the project
    correctly when bootup is run.

--- librehq/__init__.py
+++ librehq/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'This is a stub'
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
+
+from librehq import main
+import wikis
+
+app.register_blueprint(main.bp);
+app.register_blueprint(wikis.bp);
