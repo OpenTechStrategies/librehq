0b94cba2c205ed4fd081377606ca98b57346862a
Frank Duncan <frank@kank.net>
2018-10-29 14:55:06 -0500

    Add index with partial loading from submodules.
    Also requires an update to wikis to pull in the submodule code to work.

--- librehq/__init__.py
+++ librehq/__init__.py
@@ -2,11 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
-
 from librehq import main
 import wikis
 
 app.register_blueprint(main.bp);
 app.register_blueprint(wikis.bp);
+
+if __name__ == "__librehq__":
+    app.run(host='0.0.0.0')
