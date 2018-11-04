86ebc56340c7ec1f52c9fd1d57c899963a92018a
Frank Duncan <frank@kank.net>
2018-11-04 04:24:46 -0600

    Signup adds a user to the database

--- librehq/account.py
+++ librehq/account.py
@@ -9,6 +9,14 @@ bp = Blueprint('account', __name__, url_prefix='/')
 @bp.route('/signup', methods=(["POST", "GET"]))
 def signup():
     if request.method == "POST":
+        new_account = Account(username=request.form["username"],
+                              #TODO: Stored as plain text!  Change before launch!
+                              #See model definition as to logic
+                              password=request.form["password"],
+                              email=request.form["email"])
+        db.session.add(new_account)
+        db.session.commit()
+
         msg = Message("Validate",
                       sender="bot@librehq.com",
                       recipients=[request.form["email"]])
@@ -21,5 +29,15 @@ def signup():
 class Account(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(128))
+
+    # Right now, this is being stored as plain text!
+    # TODO: Hash this correctly
+    #
+    # Because this password is going to be used in subservices
+    # for authentication until we have correct authentication set up,
+    # we either need to query the user each time they want to do
+    # something that would pass through their password, or store it
+    # as plain text so we can reuse.  We choose the second so that
+    # while prototyping, the user experience matches the end goal.
     password = db.Column(db.String(128))
     email = db.Column(db.String(128))
