cde2bb6de893bf2e8c2e5c5053d75395bd45e63d
Paul Morris <paul@paulwmorris.com>
2018-11-29 16:11:54 -0500

    Reformat Header.vue to avoid long lines

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="container">
     <header>
-      <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+      <nav
+        class="navbar is-primary"
+        role="navigation"
+        aria-label="main navigation"
+      >
 
         <div class="navbar-brand">
-          <a class="navbar-item has-text-weight-bold" href="/">LibreHQ <em>beta</em></a>
-            <a v-if="loggedIn" role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="mainNavbar">
+          <a
+            class="navbar-item has-text-weight-bold"
+            href="/"
+          >LibreHQ <em>beta</em></a>
+            <a v-if="loggedIn"
+              role="button"
+              class="navbar-burger burger"
+              aria-label="menu"
+              aria-expanded="false"
+            >
               <span aria-hidden="true"></span>
               <span aria-hidden="true"></span>
               <span aria-hidden="true"></span>
             </a>
         </div>
 
-        <div v-if="loggedIn" id="mainNavbar" class="navbar-menu">
+        <div
+          v-if="loggedIn"
+          id="mainNavbar"
+          class="navbar-menu"
+        >
 
           <div class="navbar-start">
             <a class="navbar-item" href="/">
