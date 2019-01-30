dabaf14402df432f483dfa56a5305db8f1d23bb3
Paul Morris <paul@paulwmorris.com>
2019-01-30 15:52:20 -0500

    Merge 'Sign Up' and 'Sign In' widgets

--- client/src/components/Signin.vue
+++ client/src/components/Signin.vue
@@ -18,10 +18,24 @@
           </p>
         </div>
 
-        <div class="column">
-          <div class="title is-4">Sign up</div>
-          <form action="signup" method="post">
+        <div class="column"></div>
 
+        <div class="column">
+          <div class="tabs is-large is-boxed is-fullwidth">
+            <ul>
+              <li v-bind:class="signUp && 'is-active'">
+                <a @click="showSignUp">Sign Up</a>
+              </li>
+              <li v-bind:class="!signUp && 'is-active'">
+                <a @click="showSignIn">Sign In</a>
+              </li>
+            </ul>
+          </div>
+
+          <form
+            v-bind:action="signUp ? 'signup' : 'signin'"
+            method="post"
+          >
             <div class="field">
               <label class="label">Username</label>
               <div class="control has-icons-left has-icons-right">
@@ -42,7 +56,10 @@
               </div>
             </div>
 
-            <div class="field">
+            <div
+              class="field"
+              v-if="signUp"
+            >
               <label class="label">Email</label>
               <div class="control has-icons-left has-icons-right">
                 <input class="input" type="email" name="email" placeholder="Email Address">
@@ -54,42 +71,14 @@
 
             <div class="field">
               <div class="control">
-                <input class="button is-link" type="submit" value="Sign Up">
+                <input
+                  class="button is-link"
+                  type="submit"
+                  v-bind:value="signUp ? 'Sign Up' : 'Sign In'"
+                >
               </div>
             </div>
 
-          </form>
-        </div>
-
-        <div class="column">
-          <div class="title is-4">Sign in</div>
-          <form action="signin" method="post">
-
-            <div class="field">
-              <label class="label">Username</label>
-              <div class="control has-icons-left has-icons-right">
-                <input class="input" type="text" name="username" placeholder="Username">
-                <span class="icon is-small is-left">
-                  <font-awesome-icon icon="user"></font-awesome-icon>
-                </span>
-              </div>
-            </div>
-
-            <div class="field">
-              <label class="label">Password</label>
-              <div class="control has-icons-left has-icons-right">
-                <input class="input" type="password" name="password" placeholder="Password">
-                <span class="icon is-small is-left">
-                  <font-awesome-icon icon="key"></font-awesome-icon>
-                </span>
-              </div>
-            </div>
-
-            <div class="field">
-              <div class="control">
-                <input class="button is-link" type="submit" value="Sign In">
-              </div>
-            </div>
           </form>
         </div>
 
@@ -100,6 +89,19 @@
 
 <script>
 export default {
-  name: "Signin"
+  name: "Signin",
+  data() {
+    return {
+      signUp: true
+    };
+  },
+  methods: {
+    showSignUp() {
+      this.signUp = true;
+    },
+    showSignIn() {
+      this.signUp = false;
+    }
+  }
 };
 </script>
