d50e628e96eef3f15e934cd8868490219450552d
Paul Morris <paul@paulwmorris.com>
2019-01-31 14:30:41 -0500

    Add preliminary csv2wiki config form UI

--- client/src/components/Wikis.vue
+++ client/src/components/Wikis.vue
@@ -144,6 +144,11 @@
                   Populate the Wiki with CSV Data
                 </label>
               </div>
+              <!--
+                Bulma does form spacing via sibling divs with a class of
+                'field', so conditionally include each 'field' div, rather than
+                wrapping them in a conditional wrapper (which breaks spacing).
+              -->
               <div class="field" v-if="createWikiFromCsv">
                 <FileUploadInput
                   inputLabel="Choose a CSV file…"
@@ -151,11 +156,128 @@
                 />
               </div>
               <div class="field" v-if="createWikiFromCsv">
+                <div class="control">
+                  <label class="radio">
+                    <input
+                      type="radio"
+                      name="configuration"
+                      value="yesUseConfigForm"
+                      v-model="useConfigForm"
+                      checked
+                    />
+                    Enter config details
+                  </label>
+                  <br/>
+                  <label class="radio">
+                    <input
+                      type="radio"
+                      name="configuration"
+                      value=""
+                      v-model="useConfigForm"
+                    />
+                    Upload a config file
+                  </label>
+                </div>
+              </div>
+
+              <div class="field" v-if="createWikiFromCsv && useConfigForm">
+                <label class="label">Page Section Layout</label>
+                <div class="field">
+                  Each wiki page corresponds to a row in the CSV. The
+                  "Page Section Layout" shows the common structure for the
+                  pages: what the section nesting is, and which
+                  columns go in which sections in what order.
+                  Each line has one of two forms:
+               </div>
+               <div class="field">
+                  1. If it begins with one or more dots ("."),
+                  it indicates the start of a new section.
+                  The number of dots indicates the nesting
+                  level: more dots means deeper subsection.
+                  After the dots comes an optional section
+                  title.  If you put "{N}" (N is an integer)
+                  in that title, it will be replaced with the
+                  column heading for column N.  Note this
+                  uses 1-based indexing: the first column is
+                  column 1, and there is no column 0.
+                </div>
+                <div class="field">
+                  2. If it starts with a number, then the
+                  current row's content for that column
+                  number is inserted into the page here.
+                  Everything after the number is ignored.
+                  Typically, one puts a comment there
+                  describing the column that corresponds to
+                  that number.
+                </div>
+                <div class="control">
+                  <textarea
+                    class="textarea"
+                    placeholder="Page Section Layout"
+                    name="pageSectionLayout"
+                    rows="10"
+                  ></textarea>
+                </div>
+              </div>
+
+              <div class="field" v-if="createWikiFromCsv && useConfigForm">
+                <label class="label">Page Title Template</label>
+                <div class="field">
+                  The template into which selected column values from
+                  a row are substituted in order to create the title of
+                  the page generated from that row. Columns are specified
+                  with "{N}" in the string, where N is a column number.
+                  The special column {0} is the CSV row number,
+                  which is automatically left-padded with zeros
+                  appropriately for the total number of rows.
+                </div>
+                <div class="control">
+                  <input
+                    class="input"
+                    type="text"
+                    placeholder="Page Title Template"
+                    name="pageTitleTemplate"
+                  />
+                </div>
+              </div>
+
+              <div class="field" v-if="createWikiFromCsv && useConfigForm">
+                <label class="label">Title for the 'Table of Contents' Page</label>
+                <div class="control">
+                  <input
+                    class="input"
+                    type="text"
+                    placeholder="TOC"
+                    value="TOC"
+                    name="toc"
+                  >
+                </div>
+              </div>
+
+              <div class="field" v-if="createWikiFromCsv && useConfigForm">
+                <label class="label">Category Column</label>
+                <div class="field">
+                  The number of the column (if any) in the CSV
+                  file that should be used to create a category
+                  for that row. Column numbering begins at 1, not
+                  0. Leave blank to not use categories.
+                </div>
+                <div class="control">
+                  <input
+                    class="input"
+                    type="number"
+                    name="categoryColumn"
+                  />
+                </div>
+              </div>
+
+              <div class="field" v-if="createWikiFromCsv && !useConfigForm">
                 <FileUploadInput
                   inputLabel="Choose a Config file…"
                   inputName="config"
                 />
               </div>
+
               <div class="field">
                 <div class="control">
                   <input
@@ -165,6 +287,7 @@
                   >
                 </div>
               </div>
+
             </form>
           </div>
         </div>
@@ -185,7 +308,9 @@ export default {
   data() {
     return {
       wikis: [],
-      createWikiFromCsv: false
+      createWikiFromCsv: false,
+      // gets set to empty string to match radio value & provide boolean behavior
+      useConfigForm: "yesUseConfigForm"
     };
   },
   methods: {
